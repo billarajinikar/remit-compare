d01b4711afe6acda8e55022a583f935f4c1ab0b7
Rajinikar Billa <billarajinikar@gmail.com>
2025-07-12 09:48:36 +0200

    Update - google tag analytics

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -25,3 +25,12 @@
 
 
 </head>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-GPJRR85RMF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-GPJRR85RMF');
+</script>
