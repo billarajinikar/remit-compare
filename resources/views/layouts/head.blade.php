5cadf7ba87620fbbbe202aba8770e0340449408f
Rajinikar Billa <billarajinikar@gmail.com>
2025-07-12 10:56:32 +0200

    Update

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -1,11 +1,16 @@
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Pravasify - Compare best SEK TO INR Money transfer services - best remittance compare</title>
-    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="x-ua-compatible" content="IE=edge">
+    <title>Pravasify - Compare Best SEK to INR Money Transfer Services</title>
 
+    <meta name="description"
+        content="Pravasify helps Indians in Sweden compare trusted SEK to INR money transfer services to save on fees and get better exchange rates. Built by NRIs, for NRIs.">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="keywords"
+        content="SEK to INR, Money Transfer, Remittance Comparison, NRIs in Sweden, Pravasify, Send Money to India, Remit India">
+    <meta name="author" content="Pravasify">
+    <meta name="robots" content="index, follow">
+    <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
     <!--=== Reset Css ===-->
     <link rel="stylesheet" href="/assets/css/normalize.css">
     <!--=== Bootstrap ===-->
@@ -29,9 +34,9 @@
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-GPJRR85RMF"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
 
-  gtag('config', 'G-GPJRR85RMF');
+    gtag('config', 'G-GPJRR85RMF');
 </script>
